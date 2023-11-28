ab899d342817183bb2d909d56e7effdaa442643f
RajeshDhal <151999633+RajeshDhal@users.noreply.github.com>
2023-11-28 10:18:43 +0000

    IntroductionFile Saved

---
+++ basic/1st.js
@@ -0,0 +1,9 @@
+console.log("Hii")
+/*
+1.js is used for backend and frontend.
+2.It is a high level language.
+3.We use js or client side as well as server side.
+4.In js we do logical impression in web pages.
+5.In today era we use Esb version of javascript , as it is latest one.
+*ES6-Echema Script 6(Vanila JS).
+*/
